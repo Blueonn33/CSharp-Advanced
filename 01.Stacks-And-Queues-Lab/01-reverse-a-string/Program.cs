2d63dab1bcb0c35b4b15f83436c8d72a08f70d41
Martin Marinov <m.marinov1733@gmail.com>
2026-01-07 23:30:16 +0200

    Add project files.

---
+++ 01.Stacks-And-Queues-Lab/01-reverse-a-string/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01_reverse_a_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
