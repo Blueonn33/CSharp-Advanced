faea02470e142ee2ea5eedf858b4f93f14048c59
Martin Marinov <m.marinov1733@gmail.com>
2026-01-08 17:29:54 +0200

    matching brackets

---
+++ 01.Stacks-And-Queues-Lab/04-matching-brackets/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _04_matching_brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+
+            Stack<int> openingBracketsIndex = new();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    openingBracketsIndex.Push(i);
+                }
+
+                if (expression[i] == ')')
+                {
+                    int openIndex = openingBracketsIndex.Pop();
+                    string subExpression = expression.Substring(openIndex, i - openIndex + 1);
+                    Console.WriteLine(subExpression);
+                }
+            }
+        }
+    }
+}
