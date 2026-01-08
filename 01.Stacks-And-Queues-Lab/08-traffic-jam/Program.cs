f5227d4c943d6344f9c7811ad6473a866268faa4
Martin Marinov <m.marinov1733@gmail.com>
2026-01-08 19:41:35 +0200

    traffic jam

---
+++ 01.Stacks-And-Queues-Lab/08-traffic-jam/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Data;
+
+namespace _08_traffic_jam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int passedCars = 0;
+
+            Queue<string> cars = new();
+            string command = "";
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                if (command == "green")
+                {
+                    int count = Math.Min(n, cars.Count);
+
+                    for (int i = 0; i < count; i++)
+                    {
+                        string car = cars.Dequeue();
+                        Console.WriteLine($"{car} passed!");
+                        passedCars++;
+                    }
+                }
+                else
+                {
+                    cars.Enqueue(command);
+                }
+            }
+
+            Console.WriteLine($"{passedCars} cars passed the crossroads.");
+        }
+    }
+}
