2dad3d0cb31bb44ba63537dcf2b8414545b55445
Martin Marinov <m.marinov1733@gmail.com>
2026-01-08 19:03:05 +0200

    hot potato

---
+++ 01.Stacks-And-Queues-Lab/07-hot-potato/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _07_hot_potato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kids = Console.ReadLine().Split();
+
+            Queue<string> queue = new(kids);
+            int passes = int.Parse(Console.ReadLine());
+            int tosses = 1;
+
+            while (queue.Count > 1)
+            {
+                string kid = queue.Dequeue();
+
+                if (tosses == passes)
+                {
+                    tosses = 1;
+                    Console.WriteLine($"Removed {kid}");
+                }
+                else
+                {
+                    queue.Enqueue(kid);
+                    tosses++;
+                }
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
