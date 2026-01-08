99e768db352f18cdf3061548d09fefe19a70e109
Martin Marinov <m.marinov1733@gmail.com>
2026-01-08 13:45:39 +0200

    reversed string

--- 01.Stacks-And-Queues-Lab/01-reverse-a-string/Program.cs
+++ 01.Stacks-And-Queues-Lab/01-reverse-a-string/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string input = Console.ReadLine();
+
+            Stack<char> stack = new();
+
+            foreach (char c in input)
+            {
+                stack.Push(c);
+            }
+
+            foreach (var c in stack)
+            {
+                Console.Write(c);
+            }
         }
     }
 }
