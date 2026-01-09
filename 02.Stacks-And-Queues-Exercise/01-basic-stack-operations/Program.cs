2b451f549a1fe02a8302f4c29a169e4e02e2ed74
Martin Marinov <m.marinov1733@gmail.com>
2026-01-09 10:10:35 +0200

    stack operations

---
+++ 02.Stacks-And-Queues-Exercise/01-basic-stack-operations/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _01_basic_stack_operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int count = numbers[0];
+            int pop = numbers[1];
+            int check = numbers[2];
+
+            Stack<int> stack = new();
+
+            int[] elements = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < count; i++)
+            {
+                stack.Push(elements[i]);
+            }
+
+            for (int i = 0; i < pop; i++)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Contains(check))
+            {
+                Console.WriteLine("true");
+            }
+            else if (stack.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(stack.Min());
+            }
+        }
+    }
+}
