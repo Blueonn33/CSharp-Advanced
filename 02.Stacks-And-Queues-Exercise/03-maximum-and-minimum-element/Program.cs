ce550d641222d0b8c1787c4607299100b61c34a8
Martin Marinov <m.marinov1733@gmail.com>
2026-01-09 12:11:52 +0200

    min / max element

---
+++ 02.Stacks-And-Queues-Exercise/03-maximum-and-minimum-element/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _03_maximum_and_minimum_element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Stack<int> stack = new();
+            string command = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                command = Console.ReadLine();
+                string[] parts = command.Split();
+                string numberCommand = parts[0];
+
+                switch (numberCommand)
+                {
+                    case "1":
+                        int numberToPush = int.Parse(parts[1]);
+                        stack.Push(numberToPush);
+                        break;
+                    case "2":
+                        stack.Pop();
+                        break;
+                    case "3":
+
+                        if (stack.Count > 0)
+                        {
+                            Console.WriteLine(stack.Max());
+                        }
+
+                        break;
+                    case "4":
+
+                        if (stack.Count > 0)
+                        {
+                            Console.WriteLine(stack.Min());
+                        }
+
+                        break;
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", stack));
+        }
+    }
+}
