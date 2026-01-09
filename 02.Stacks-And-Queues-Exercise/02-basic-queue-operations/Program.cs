5fe96cf30e427d63e668092de6ce1bf97a34e926
Martin Marinov <m.marinov1733@gmail.com>
2026-01-09 12:03:28 +0200

    queue operations

---
+++ 02.Stacks-And-Queues-Exercise/02-basic-queue-operations/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _02_basic_queue_operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int count = numbers[0];
+            int enqueue = numbers[1];
+            int check = numbers[2];
+
+            Queue<int> queue = new();
+
+            int[] elements = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < count; i++)
+            {
+                queue.Enqueue(elements[i]);
+            }
+
+            for (int i = 0; i < enqueue; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Contains(check))
+            {
+                Console.WriteLine("true");
+            }
+            else if (queue.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(queue.Min());
+            }
+        }
+    }
+}
