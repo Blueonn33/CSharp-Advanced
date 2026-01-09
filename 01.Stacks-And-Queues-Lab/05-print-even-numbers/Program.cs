597522dac8ae8af5b58de40939bff52991c9ae72
Martin Marinov <m.marinov1733@gmail.com>
2026-01-09 09:35:54 +0200

    print even numbers

---
+++ 01.Stacks-And-Queues-Lab/05-print-even-numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _05_print_even_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> queue = new(numbers);
+            Queue<int> evenNumbers = new();
+
+            foreach (var item in queue)
+            {
+                if (item % 2 == 0)
+                {
+                    evenNumbers.Enqueue(item);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", evenNumbers));
+        }
+    }
+}
