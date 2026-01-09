48c92d94a3463a313564249eb3c4e4aca47bf7ff
Martin Marinov <m.marinov1733@gmail.com>
2026-01-09 21:42:20 +0200

    fast food

---
+++ 02.Stacks-And-Queues-Exercise/04-fast-food/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _04_fast_food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodAmount = int.Parse(Console.ReadLine());
+
+            int[] ordersInput = Console.ReadLine()
+                                .Split()
+                                .Select(int.Parse)
+                                .ToArray();
+
+            Queue<int> orders = new(ordersInput);
+
+            int maxOrder = orders.Max();
+
+            while (orders.Count > 0)
+            {
+                int order = orders.Peek();
+
+                if (order <= foodAmount)
+                {
+                    foodAmount -= order;
+                    orders.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(maxOrder);
+
+            if (orders.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.Write("Orders left: ");
+                Console.Write(string.Join(" ", orders));
+            }
+        }
+    }
+}
