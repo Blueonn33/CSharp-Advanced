900e4a65bf6c02717e11c58b9cebd1231aa91ba4
Martin Marinov <m.marinov1733@gmail.com>
2026-01-09 09:12:12 +0200

    calculator

--- 01.Stacks-And-Queues-Lab/03-simple-calculator/Program.cs
+++ 01.Stacks-And-Queues-Lab/03-simple-calculator/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] input = Console.ReadLine().Split().Reverse().ToArray();
+
+            Stack<string> stack = new(input);
+
+            int result = int.Parse(stack.Pop());
+
+            while (stack.Count > 1)
+            {
+                string operation = stack.Pop();
+                int number = int.Parse(stack.Pop());
+
+                if (operation == "+")
+                {
+                    result += number;
+                }
+                else if (operation == "-")
+                {
+                    result -= number;
+                }
+            }
+
+            Console.WriteLine(result);
         }
     }
 }
