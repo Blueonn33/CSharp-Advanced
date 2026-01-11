bbd7201da3d82121cb6a0a841da2b35a923c1526
Martin Marinov <m.marinov1733@gmail.com>
2026-01-11 23:35:40 +0200

    sum columns

---
+++ 03.Multidimensional-Arrays-Lab/02-sum-matrix-columns/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+int[] matrixDimensions = Console.ReadLine()
+    .Split(", ")
+    .Select(int.Parse)
+    .ToArray();
+
+int rows = matrixDimensions[0];
+int cols = matrixDimensions[1];
+
+int[,] matrix = ReadMatrix(rows, cols);
+
+for (int col = 0; col < cols; col++)
+{
+    int sum = 0;
+
+    for (int row = 0; row < rows; row++)
+    {
+        sum += matrix[row, col];
+    }
+
+    Console.WriteLine(sum);
+}
+
+static int[,] ReadMatrix(int rows, int cols)
+{
+    var matrix = new int[rows, cols];
+
+    for (int row = 0; row < rows; row++)
+    {
+        int[] currentRow = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+
+        for (int col = 0; col < cols; col++)
+        {
+            matrix[row, col] = currentRow[col];
+        }
+    }
+
+    return matrix;
+}
