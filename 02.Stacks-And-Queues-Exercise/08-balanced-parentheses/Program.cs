381b0aa9c1bd86ba54f20f9b7072aab7fea253c0
Martin Marinov <m.marinov1733@gmail.com>
2026-01-11 15:50:08 +0200

    brackets

--- 02.Stacks-And-Queues-Exercise/08-balanced-parentheses/Program.cs
+++ 02.Stacks-And-Queues-Exercise/08-balanced-parentheses/Program.cs
@@ -4,7 +4,60 @@
     {
         static void Main(string[] args)
         {
-            
+            char[] expression = Console.ReadLine().ToCharArray();
+
+            Stack<char> openBrackets = new();
+            bool isBalanced = true;
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(' || expression[i] == '{' || expression[i] == '[')
+                {
+                    openBrackets.Push(expression[i]);
+                }
+                else
+                {
+                    if (openBrackets.Count == 0)
+                    {
+                        isBalanced = false;
+                        break;
+                    }
+
+                    char openBracket = openBrackets.Pop();
+
+                    switch (expression[i])
+                    {
+                        case ')':
+                            if (openBracket != '(')
+                            {
+                                isBalanced = false;
+                            }
+
+                            break;
+                        case '}':
+                            if (openBracket != '{')
+                            {
+                                isBalanced = false;
+                            }
+
+                            break;
+                        case ']':
+                            if (openBracket != '[')
+                            {
+                                isBalanced = false;
+                            }
+
+                            break;
+                    }
+
+                    if (!isBalanced)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(isBalanced ? "YES" : "NO");
         }
     }
 }
