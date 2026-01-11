263c933258afb2baf1563069a4d32e7a06306dd8
Martin Marinov <m.marinov1733@gmail.com>
2026-01-11 18:32:24 +0200

    cars

--- 02.Stacks-And-Queues-Exercise/10-crossroads/Program.cs
+++ 02.Stacks-And-Queues-Exercise/10-crossroads/Program.cs
@@ -4,40 +4,72 @@
     {
         static void Main(string[] args)
         {
-            int greenLightDuration = int.Parse(Console.ReadLine());
-            int freeWindowDuration = int.Parse(Console.ReadLine());
-            int passed = 0;
-            int green = 0;
+            int greenLightSeconds = int.Parse(Console.ReadLine());
+            int windowSeconds = int.Parse(Console.ReadLine());
+            bool isCrash = false;
+            int passedCars = 0;
 
             Queue<string> cars = new();
 
             string command = "";
 
-            while ((command = Console.ReadLine()) != "END")
+            while (command.ToLower() != "end")
             {
-                if (command == "green")
+                command = Console.ReadLine().Trim();
+
+                if (command.ToLower() == "green" && cars.Count > 0)
                 {
-                    green = greenLightDuration; 
-                }
-                else
-                { 
-                    cars.Enqueue(command);
+                    int greenLight = greenLightSeconds;
+                    int window = windowSeconds;
+                    string car = cars.Dequeue();
+                    passedCars++;
+                    Queue<char> carSymbols = new(car);
 
-                    if (green >= command.Length)
+                    while (greenLight > 0)
                     {
-                        cars.Dequeue();
-                        passed++;
-                        green -= command.Length;
-                    }
-                    else
-                    {
-                        if (command.Length - green > 0)
+                        if (carSymbols.Count == 0)
                         {
-                            command = command.Substring(0, green);
+                            if (cars.Count == 0)
+                            {
+                                break;
+                            }
 
+                            car = cars.Dequeue();
+                            passedCars++;
+                            carSymbols = new Queue<char>(car);
+                        }
+
+                        carSymbols.Dequeue();
+                        greenLight--;
+                    }
+
+                    if (carSymbols.Count > 0)
+                    {
+                        while (window > 0 && carSymbols.Count > 0)
+                        {
+                            carSymbols.Dequeue();
+                            window--;
+                        }
+
+                        if (carSymbols.Count > 0)
+                        {
+                            isCrash = true;
+                            Console.WriteLine("A crash happened!");
+                            Console.WriteLine($"{car} was hit at {carSymbols.Dequeue()}.");
+                            break;
                         }
                     }
                 }
+                else if (command.ToLower() != "green" && command.ToLower() != "end")
+                {
+                    cars.Enqueue(command);
+                }
+            }
+
+            if (!isCrash)
+            {
+                Console.WriteLine("Everyone is safe.");
+                Console.WriteLine($"{passedCars} total cars passed the crossroads.");
             }
         }
     }
