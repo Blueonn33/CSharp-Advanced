92ed896d7506adfee5559d57b96cc505f4eaa63b
Martin Marinov <m.marinov1733@gmail.com>
2026-01-11 23:19:11 +0200

    sum matrix

---
+++ 03.Multidimensional-Arrays-Lab/01-sum-matrix-elements/Program.cs
@@ -0,0 +1,49 @@
+﻿int[] matrixDimensions = Console.ReadLine()
+    .Split(", ")
+    .Select(int.Parse)
+    .ToArray();
+
+int rows = matrixDimensions[0];
+int cols = matrixDimensions[1];
+
+int[,] matrix = ReadMatrix(rows, cols);
+int sum = MatrixSum(matrix);
+
+Console.WriteLine(rows);
+Console.WriteLine(cols);
+Console.WriteLine(sum);
+
+static int MatrixSum(int[,] matrix)
+{
+    int sum = 0;
+
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        for (int col = 0; col < matrix.GetLength(1); col++)
+        {
+            sum += matrix[row, col];
+        }
+    }
+
+    return sum;
+}
+
+static int[,] ReadMatrix(int rows, int cols)
+{
+    var matrix = new int[rows, cols];
+
+    for (int row = 0; row < rows; row++)
+    {
+        int[] currentRow = Console.ReadLine()
+            .Split(", ")
+            .Select(int.Parse)
+            .ToArray();
+
+        for (int col = 0; col < cols; col++)
+        {
+            matrix[row, col] = currentRow[col];
+        }
+    }
+
+    return matrix;
+}
---
+++ 03.Multidimensional-Arrays-Lab/Playground/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace Playground
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[2, 3]
+            {
+                {17, 33, 22},
+                {45, 12, 9}
+            };
+
+            string[,,] cube = new string[2, 2, 2];
+
+            matrix[1, 1] = 17;
+
+            //Console.WriteLine(matrix[1, 1]);
+
+            int[,] matrix2 =
+            {
+                {1,2},
+                {3,4},
+                {5,6}
+            };
+
+            var arr = new[] { 1, 2, 3, 4 };
+
+            //Console.WriteLine(matrix.GetLength(0)); // number of rows
+            //Console.WriteLine(matrix.GetLength(1)); // number of columns
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            string[,] textMatrix = new string[2, 5];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    textMatrix[row, col] = $"[{row},{col}]";
+                }
+            }
+
+            PrintMatrix(textMatrix);
+        }
+        static void PrintMatrix(string[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void PrintMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static int[,] ReadMatrix()
+        {
+            Console.Write("Enter number of rows: ");
+            int rows = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter number of columns: ");
+            int cols = int.Parse(Console.ReadLine());
+
+            var matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write($"Enter [{row},{col}]: ");
+
+                    int value = int.Parse(Console.ReadLine());
+                    matrix[row, col] = value;
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
