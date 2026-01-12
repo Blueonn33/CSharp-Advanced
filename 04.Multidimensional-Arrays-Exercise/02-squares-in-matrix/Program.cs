43d8a87fa363ae4c0315da1f33704f460d692313
Martin Marinov <m.marinov1733@gmail.com>
2026-01-12 13:38:51 +0200

    squares in matrix

---
+++ 04.Multidimensional-Arrays-Exercise/02-squares-in-matrix/Program.cs
@@ -0,0 +1,50 @@
+﻿int[] input = Console.ReadLine()!
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int rows = input[0];
+int cols = input[1];
+
+if (rows < 2 || cols < 2)
+{
+    Console.WriteLine(0);
+}
+else
+{
+    string[,] matrix = new string[rows, cols];
+
+    for (int row = 0; row < rows; row++)
+    {
+        string[] symbols = Console.ReadLine()
+            .Split()
+            .ToArray();
+
+        for (int col = 0; col < cols; col++)
+        {
+            matrix[row, col] = symbols[col];
+        }
+    }
+
+    int counter = 0;
+
+    for (int row = 0; row < rows - 1; row++)
+    {
+        for (int col = 0; col < cols - 1; col++)
+        {
+            bool found = matrix[row, col] ==
+                         matrix[row, col + 1] &&
+                         matrix[row, col] ==
+                         matrix[row + 1, col] &&
+                         matrix[row, col] ==
+                         matrix[row + 1, col + 1];
+
+            if (found)
+            {
+                counter++;
+            }
+        }
+    }
+
+    Console.WriteLine(counter);
+}
