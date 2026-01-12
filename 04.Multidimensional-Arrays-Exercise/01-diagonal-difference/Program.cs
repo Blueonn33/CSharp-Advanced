ffc178676fc204ad8e2d4037c0f6a17e2d6b84a6
Martin Marinov <m.marinov1733@gmail.com>
2026-01-12 13:07:30 +0200

    diagonal difference

---
+++ 04.Multidimensional-Arrays-Exercise/01-diagonal-difference/Program.cs
@@ -0,0 +1,37 @@
+﻿int size = int.Parse(Console.ReadLine()!);
+
+int[,] matrix = new int[size, size];
+matrix = ReadMatrix(size);
+
+int primaryDiagonalSum = 0;
+int secondaryDiagonalSum = 0;
+
+for (int row = 0, row1 = size - 1; row < size; row++, row1--)
+{
+    primaryDiagonalSum += matrix[row, row];
+    secondaryDiagonalSum += matrix[row1, row];
+}
+
+int result = Math.Abs(primaryDiagonalSum - secondaryDiagonalSum);
+
+Console.WriteLine(result);
+
+static int[,] ReadMatrix(int size)
+{
+    int[,] matrix = new int[size, size];
+
+    for (int row = 0; row < size; row++)
+    {
+        int[] rowData = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+
+        for (int col = 0; col < size; col++)
+        {
+            matrix[row, col] = rowData[col];
+        }
+    }
+
+    return matrix;
+}
