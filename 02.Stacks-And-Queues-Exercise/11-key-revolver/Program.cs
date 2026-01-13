069e33ff0c96ba5ac15e99492a70b277f4ada703
Martin Marinov <m.marinov1733@gmail.com>
2026-01-13 10:30:46 +0200

    bullets

--- 02.Stacks-And-Queues-Exercise/11-key-revolver/Program.cs
+++ 02.Stacks-And-Queues-Exercise/11-key-revolver/Program.cs
@@ -4,7 +4,58 @@
     {
         static void Main(string[] args)
         {
-            
+            int bulletPrice = int.Parse(Console.ReadLine());
+            int gunBarrelSize = int.Parse(Console.ReadLine());
+            int[] bulletsInput = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] locksInput = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int intelligenceValue = int.Parse(Console.ReadLine());
+
+            Stack<int> bullets = new(bulletsInput);
+            Queue<int> locks = new(locksInput);
+
+            int barrelCounter = 0;
+
+            while (bullets.Count > 0 && locks.Count > 0)
+            {
+                int bullet = bullets.Pop();
+                int currentLock = locks.Peek();
+                barrelCounter++;
+
+                if (bullet <= currentLock)
+                {
+                    Console.WriteLine("Bang!");
+                    locks.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+                }
+
+                if (barrelCounter == gunBarrelSize && bullets.Count > 0)
+                {
+                    Console.WriteLine("Reloading!");
+                    barrelCounter = 0;
+                }
+            }
+
+            if (bullets.Count >= 0 && locks.Count == 0)
+            {
+                int moneyEarned = intelligenceValue - (bulletPrice * (bulletsInput.Length - bullets.Count));
+
+                Console.WriteLine($"{bullets.Count} bullets left. Earned ${moneyEarned}");
+            }
+            else
+            {
+                Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+            }
         }
     }
 }
