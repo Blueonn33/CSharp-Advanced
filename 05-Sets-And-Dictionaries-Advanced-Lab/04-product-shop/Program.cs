86ba43c38b21e254f6db6e85e4ee0e3b4babff95
Martin Marinov <m.marinov1733@gmail.com>
2026-01-14 21:44:42 +0200

    shop

---
+++ 05-Sets-And-Dictionaries-Advanced-Lab/04-product-shop/Program.cs
@@ -0,0 +1,33 @@
+﻿var shops = new SortedDictionary<string, Dictionary<string, double>>();
+
+while (true)
+{
+    var command = Console.ReadLine();
+
+    if (command == "Revision")
+    {
+        break;
+    }
+
+    var commandParts = command.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+    var shop = commandParts[0];
+    var product = commandParts[1];
+    var price = double.Parse(commandParts[2]);
+
+    if (!shops.ContainsKey(shop))
+    {
+        shops[shop] = new Dictionary<string, double>();
+    }
+
+    shops[shop][product] = price;
+}
+
+foreach (var (shop, products) in shops)
+{
+    Console.WriteLine($"{shop}->");
+
+    foreach (var (product, price) in products)
+    {
+        Console.WriteLine($"Product: {product}, Price: {price}");
+    }
+}
