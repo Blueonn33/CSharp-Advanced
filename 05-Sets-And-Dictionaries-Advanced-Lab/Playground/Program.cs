063d07d2c0239d6acd0e72299b4135a19ea67513
Martin Marinov <m.marinov1733@gmail.com>
2026-01-14 13:26:22 +0200

    dictionary

---
+++ 05-Sets-And-Dictionaries-Advanced-Lab/Playground/Program.cs
@@ -0,0 +1,39 @@
+﻿// (string) name , (string) phoneNumber
+Dictionary<string, string> phoneNumbers = new();
+
+// name (string), scores (double)
+Dictionary<string, double> scores = new();
+
+scores["Martin"] = 6;
+scores["Alex"] = 4.5;
+scores["John"] = 3.5;
+scores["Mary"] = 5.5;
+scores["Steve"] = 4.5;
+
+foreach (var (name, score) in scores)
+{
+    Console.WriteLine($"{name} has a score of {score}");
+}
+
+//foreach (var entry in scores)
+//{
+//    var name = entry.Key;
+//    var score = entry.Value;
+
+//    Console.WriteLine($"{name} has a score of {score}");
+//}
+
+SortedDictionary<string, double> sortedScores = new(scores);
+
+Console.WriteLine();
+Console.WriteLine();
+
+sortedScores.Add("Emiliqna", 3.4);
+sortedScores.Remove("John");
+sortedScores.Remove("Mary");
+sortedScores.Remove("Steve");
+
+foreach (var (name, score) in sortedScores)
+{
+    Console.WriteLine($"{name} has a score of {score}");
+}
