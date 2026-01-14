caac47e6d5e2bfb8af0e7297a2813f7dda8121eb
Martin Marinov <m.marinov1733@gmail.com>
2026-01-14 15:50:59 +0200

    Update Program.cs

--- 05-Sets-And-Dictionaries-Advanced-Lab/02-average-student-grades/Program.cs
+++ 05-Sets-And-Dictionaries-Advanced-Lab/02-average-student-grades/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = int.Parse(Console.ReadLine());
+Dictionary<string, List<decimal>> studentGrades = new();
+
+for (int i = 0; i < n; i++)
+{
+    string[] input = Console.ReadLine().Split(' ');
+    string name = input[0];
+    decimal grade = decimal.Parse(input[1]);
+
+    if (studentGrades.ContainsKey(name))
+    {
+        studentGrades[name].Add(grade);
+    }
+    else
+    {
+        studentGrades.Add(name, new List<decimal> { grade });
+    }
+}
+
+foreach (var student in studentGrades)
+{
+    decimal averageGrade = student.Value.Average();
+
+    Console.Write($"{student.Key} -> ");
+
+    foreach (var grade in student.Value)
+    {
+        Console.Write($"{grade:F2} ");
+    }
+
+    Console.Write($"(avg: {averageGrade:F2})");
+    Console.WriteLine();
+}
