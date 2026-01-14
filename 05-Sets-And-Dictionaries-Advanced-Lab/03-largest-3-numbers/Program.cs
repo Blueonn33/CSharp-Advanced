f62e191c6468d926490f8bfcb6af39e69ca8d9df
Martin Marinov <m.marinov1733@gmail.com>
2026-01-14 15:58:00 +0200

    top 3

---
+++ 05-Sets-And-Dictionaries-Advanced-Lab/03-largest-3-numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] numbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int[] sorted = numbers.OrderByDescending(n => n).ToArray();
+
+if (sorted.Length >= 3)
+{
+    Console.WriteLine($"{sorted[0]} {sorted[1]} {sorted[2]}");
+}
+else
+{
+    foreach (var item in sorted)
+    {
+        Console.Write($"{item} ");
+    }
+}
