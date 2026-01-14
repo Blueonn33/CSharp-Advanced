d16306b8d109ead0f8c1e8bcbb400c959ef87acc
Martin Marinov <m.marinov1733@gmail.com>
2026-01-14 15:30:37 +0200

    count duplicate

--- 05-Sets-And-Dictionaries-Advanced-Lab/01-count-same-values-in-array/Program.cs
+++ 05-Sets-And-Dictionaries-Advanced-Lab/01-count-same-values-in-array/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿double[] array = Console.ReadLine().
+    Split(' ').Select(double.Parse).ToArray();
+
+Dictionary<double, int> counts = new();
+
+foreach (var number in array)
+{
+    if (counts.ContainsKey(number))
+    {
+        counts[number]++;
+    }
+    else
+    {
+        counts.Add(number, 1);
+    }
+}
+
+foreach (var (number, count) in counts)
+{
+    Console.WriteLine($"{number} - {count} times");
+}
