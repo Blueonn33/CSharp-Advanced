e137b3332e0d8629bbe594e367ac8fac2fbb896b
Martin Marinov <m.marinov1733@gmail.com>
2026-01-15 14:37:46 +0200

    count symbols

---
+++ 06.Sets-And-Dictionaries-Advanced-Exercise/05-count-symbols/Program.cs
@@ -0,0 +1,22 @@
+﻿string input = Console.ReadLine();
+
+Dictionary<char, int> symbolCounts = new Dictionary<char, int>();
+
+char[] symbols = input.ToCharArray();
+
+for (int i = 0; i < symbols.Length; i++)
+{
+    if (symbolCounts.ContainsKey(symbols[i]))
+    {
+        symbolCounts[symbols[i]]++;
+    }
+    else
+    {
+        symbolCounts[symbols[i]] = 1;
+    }
+}
+
+foreach (var (symbol, count) in symbolCounts.OrderBy(s => s.Key))
+{
+    Console.WriteLine($"{symbol}: {count} time/s");
+}
