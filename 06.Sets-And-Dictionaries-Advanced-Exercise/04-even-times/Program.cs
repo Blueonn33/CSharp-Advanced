ada33c66feb9f70388f25a8de84f36d51ac9fbf7
Martin Marinov <m.marinov1733@gmail.com>
2026-01-15 12:02:59 +0200

    even times

---
+++ 06.Sets-And-Dictionaries-Advanced-Exercise/04-even-times/Program.cs
@@ -0,0 +1,24 @@
+﻿int n = int.Parse(Console.ReadLine());
+Dictionary<int, int> counts = new Dictionary<int, int>();
+int number = 0;
+
+for (int i = 0; i < n; i++)
+{
+    int commandInput = int.Parse(Console.ReadLine());
+
+    if (counts.ContainsKey(commandInput))
+    {
+        counts[commandInput]++;
+
+        if (counts[commandInput] % 2 == 0)
+        {
+            number = commandInput;
+        }
+    }
+    else
+    {
+        counts[commandInput] = 1;
+    }
+}
+
+Console.WriteLine(number);
