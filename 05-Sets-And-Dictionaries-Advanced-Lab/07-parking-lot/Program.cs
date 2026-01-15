37660cb8a53469267515768b4d400d30a278ba2c
Martin Marinov <m.marinov1733@gmail.com>
2026-01-15 11:07:05 +0200

    parking

---
+++ 05-Sets-And-Dictionaries-Advanced-Lab/07-parking-lot/Program.cs
@@ -0,0 +1,31 @@
+﻿string command = "";
+var parkingLot = new HashSet<string>();
+
+while ((command = Console.ReadLine()) != "END")
+{
+    string[] parts = command.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+    string direction = parts[0];
+    string carNumber = parts[1];
+
+    if (direction == "IN")
+    {
+        parkingLot.Add(carNumber);
+    }
+    else if (direction == "OUT")
+    {
+        parkingLot.Remove(carNumber);
+    }
+}
+
+if (parkingLot.Count == 0)
+{
+    Console.WriteLine("Parking Lot is Empty");
+}
+else
+{
+    foreach (var car in parkingLot)
+    {
+        Console.WriteLine(car);
+    }
+}
