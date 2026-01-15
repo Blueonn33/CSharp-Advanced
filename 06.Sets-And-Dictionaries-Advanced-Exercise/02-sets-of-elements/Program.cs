faab207430c6b35b54d9cefb7cb54cc74efb14c3
Martin Marinov <m.marinov1733@gmail.com>
2026-01-15 11:51:35 +0200

    set of elements

---
+++ 06.Sets-And-Dictionaries-Advanced-Exercise/02-sets-of-elements/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] numberOfSets = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+var firstSet = new HashSet<int>();
+var secondSet = new HashSet<int>();
+
+//4 3
+// 1
+// 3
+// 5
+// 7
+// 3
+// 4
+// 5
+
+for (int i = 0; i < numberOfSets[0]; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+    firstSet.Add(number);
+}
+
+for (int i = 0; i < numberOfSets[1]; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+    secondSet.Add(number);
+}
+
+var intersect = firstSet.Intersect(secondSet);
+
+foreach (var item in intersect)
+{
+    Console.Write(item + " ");
+}
