d82f551b2afd53f5a6a56f254c13e037fd3afb58
Martin Marinov <m.marinov1733@gmail.com>
2026-01-15 11:32:11 +0200

    unique names

---
+++ 06.Sets-And-Dictionaries-Advanced-Exercise/01-unique-usernames/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = int.Parse(Console.ReadLine());
+var uniqueNames = new HashSet<string>();
+
+for (int i = 0; i < n; i++)
+{
+    string command = Console.ReadLine();
+    uniqueNames.Add(command);
+}
+
+foreach (var name in uniqueNames)
+{
+    Console.WriteLine(name);
+}
