96834c5c834f78324515f837bc4ce3448f566965
Martin Marinov <m.marinov1733@gmail.com>
2026-01-17 18:02:45 +0200

    influencers

---
+++ 06.Sets-And-Dictionaries-Advanced-Exercise/07-the-v-logger/Program.cs
@@ -0,0 +1,60 @@
+﻿Dictionary<string, List<string>> influencers = new();
+Dictionary<string, List<string>> following = new();
+
+string input = Console.ReadLine();
+
+while (input != "Statistics")
+{
+    string[] split = input.Split();
+    string influencer = split[0];
+    string command = split[1];
+
+    if (command == "joined")
+    {
+        if (!influencers.ContainsKey(influencer))
+        {
+            influencers.Add(influencer, new List<string>());
+            following.Add(influencer, new List<string>());
+        }
+    }
+    else
+    {
+        string secondInfluencer = split[2];
+
+        if (influencers.ContainsKey(influencer) && influencers.ContainsKey(secondInfluencer))
+        {
+            if (influencer != secondInfluencer &&
+                !influencers[secondInfluencer].Contains(influencer))
+            {
+                influencers[secondInfluencer].Add(influencer);
+                following[influencer].Add(secondInfluencer);
+            }
+        }
+    }
+
+    input = Console.ReadLine();
+}
+
+influencers = influencers
+    .OrderByDescending(i => i.Value.Count)
+    .ThenBy(i => following[i.Key].Count)
+    .ToDictionary(i => i.Key, i => i.Value);
+
+Console.WriteLine($"The V-Logger has a total of {influencers.Count} vloggers in its logs.");
+
+int counter = 1;
+
+foreach (var (influencer, followers) in influencers)
+{
+    Console.WriteLine($"{counter}. {influencer} : {followers.Count} followers, {following[influencer].Count} following");
+
+    if (counter == 1)
+    {
+        foreach (var follower in followers.OrderBy(f => f))
+        {
+            Console.WriteLine($"*  {follower}");
+        }
+    }
+
+    counter++;
+}
---
+++ 06.Sets-And-Dictionaries-Advanced-Exercise/DictionarySort/Program.cs
@@ -0,0 +1,37 @@
+﻿Dictionary<int, int> dictionary = new Dictionary<int, int>()
+{
+    {5,6},
+    {3,2},
+    {4,3}, {333,4}, {43,4}, {33,4}
+};
+
+// 1-ви начин
+//Dictionary<int, int> sorted = new(dictionary.OrderBy(item => item.Key));
+
+// 2-ри начин
+dictionary = dictionary.OrderByDescending(item => item.Value)
+    .ThenByDescending(item => item.Key)
+    .ToDictionary(item => item.Key, item => item.Value);
+
+foreach (var (key, value) in dictionary)
+{
+    Console.WriteLine($"{key} - {value}");
+}
+
+Dictionary<string, List<int>> studentScores = new()
+{
+    {"Martin", [6,6,5]},
+    {"Raya", [3,5,6]},
+    {"Ema", [3,3,4]}
+};
+
+Console.WriteLine();
+
+studentScores = studentScores.OrderByDescending(item => item.Value.Average())
+    .ThenByDescending(item => item.Key)
+    .ToDictionary(item => item.Key, item => item.Value);
+
+foreach (var (key, value) in studentScores)
+{
+    Console.WriteLine($"{key} - {value.Average():F2}");
+}
