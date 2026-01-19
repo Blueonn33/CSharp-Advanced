2c08e8f43a48f50ad26b566cf1a98f9f93f86495
Martin Marinov <m.marinov1733@gmail.com>
2026-01-19 22:29:35 +0200

    playground

---
+++ 07.Streams-Files-And-Directories-Lab/Playground/Program.cs
@@ -0,0 +1,22 @@
+﻿using var writer = new StreamWriter("PlaygroundWrite.txt");
+using var reader = new StreamReader("Playground.txt");
+
+int counter = 1;
+
+while (true)
+{
+    var line = reader.ReadLine();
+
+    if (line == null)
+    {
+        break;
+    }
+
+    writer.WriteLine($"{counter}. {line}");
+    counter++;
+}
+
+Console.WriteLine("Done");
+
+// .  -> текуща директория
+// .. -> родителска директория
