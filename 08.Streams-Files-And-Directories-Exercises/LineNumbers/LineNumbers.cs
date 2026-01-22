1370cfd28b9bfc47b073ad40533fe73ec313be81
Martin Marinov <m.marinov1733@gmail.com>
2026-01-22 13:47:14 +0200

    line numbers

---
+++ 08.Streams-Files-And-Directories-Exercises/LineNumbers/LineNumbers.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+
+namespace LineNumbers
+{
+    using System;
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            int counter = 1;
+
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                using (StreamWriter writer = new StreamWriter(outputFilePath))
+                {
+                    string line = reader.ReadLine();
+
+                    while (line != null)
+                    {
+                        writer.WriteLine($"Line {counter}: {line}");
+                        counter++;
+
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
