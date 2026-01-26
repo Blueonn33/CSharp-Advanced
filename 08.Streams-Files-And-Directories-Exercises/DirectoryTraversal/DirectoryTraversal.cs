5c0d64127862d9315d6c5f600b21011c113e4cf9
Martin Marinov <m.marinov1733@gmail.com>
2026-01-26 22:27:54 +0200

    directory traversal

--- 08.Streams-Files-And-Directories-Exercises/DirectoryTraversal/DirectoryTraversal.cs
+++ 08.Streams-Files-And-Directories-Exercises/DirectoryTraversal/DirectoryTraversal.cs
@@ -1,4 +1,9 @@
-﻿namespace DirectoryTraversal
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DirectoryTraversal
 {
     using System;
     public class DirectoryTraversal
@@ -16,12 +21,47 @@
 
         public static string TraverseDirectory(string inputFolderPath)
         {
-            throw new NotImplementedException();
+            Dictionary<string, List<FileInfo>> extensions = new();
+
+            string[] filePaths = Directory.GetFiles(inputFolderPath);
+
+            foreach (var filePath in filePaths)
+            {
+                FileInfo fileInfo = new FileInfo(filePath);
+
+                if (!extensions.ContainsKey(fileInfo.Extension))
+                {
+                    extensions.Add(fileInfo.Extension, new List<FileInfo>());
+                }
+
+                extensions[fileInfo.Extension].Add(fileInfo);
+            }
+
+            extensions = extensions.OrderByDescending(e => e.Value.Count)
+                .ThenBy(e => e.Key)
+                                      .ToDictionary(e => e.Key, e => e.Value);
+
+            StringBuilder result = new();
+
+            foreach (var (extension, fileInfos) in extensions)
+            {
+                result.AppendLine($"{extension}");
+
+                foreach (var fileInfo in fileInfos.OrderByDescending(f => f.Length))
+                {
+                    result.AppendLine($"--{fileInfo.Name} - {Math.Ceiling(fileInfo.Length / 1024.0)}kb");
+                }
+            }
+
+            return result.ToString().TrimEnd();
         }
 
         public static void WriteReportToDesktop(string textContent, string reportFileName)
         {
-            throw new NotImplementedException();
+            using (StreamWriter writer = new StreamWriter(reportFileName))
+            {
+                writer.WriteLine(textContent);
+            }
         }
     }
 }
