c8cd0d9479b49f5bce2a3409604174639e1fad14
Martin Marinov <m.marinov1733@gmail.com>
2026-01-29 23:51:49 +0200

    playground

--- 09.Functional-Programming-Lab/Playground/Program.cs
+++ 09.Functional-Programming-Lab/Playground/Program.cs
@@ -1,32 +1,63 @@
-﻿class Program
+﻿//var myDelegate = new MyDelegate(PrintHello);
+
+//myDelegate += PrintGoodbye;
+
+//GetDelegate(myDelegate);
+
+//var numberDelegate = new CalculationDelegate(Sum);
+
+//var result1 = numberDelegate(5, 10);
+//Console.WriteLine($"Sum: {result1}");
+
+//static void GetDelegate(MyDelegate myDelegate)
+//{
+//    myDelegate();
+//}
+
+//static int Sum(int x, int y)
+//{
+//    return x + y;
+//}
+
+//static int Multiply(int x, int y)
+//{
+//    return x * y;
+//}
+
+//static void PrintHello()
+//{
+//    Console.WriteLine("Hello from delegate!");
+//}
+
+//static void PrintGoodbye()
+//{
+//    Console.WriteLine("Goodbye from delegate!");
+//}
+
+//delegate void MyDelegate();
+//delegate int CalculationDelegate(int x, int y);
+
+using System.Threading.Channels;
+
+Action myMethod = () => Console.WriteLine("Hello");
+
+var myPrintNameMethod = () => Console.WriteLine("John Doe");
+
+var myPrintMethod = Print;
+
+myMethod();
+
+static void ActionableMethod(Action action)
 {
-    private static int number = 0;
-    static void Main(string[] args)
-    {
-        var result = Console.ReadLine()
-            .Split()
-            .Select(int.Parse)
-            .Where(n => n % 2 == 0)
-            .ToList();
+    action();
+}
 
-        var numbers = new int[]{ 4, 3, 2, 2, 4, 56, 32, 4 };
-        var filtered = numbers
-            .Where(Filter)
-            .ToList();
-    }
+//static void PrintHello()
+//{
+//    Console.WriteLine("Hello");
+//}
 
-    static bool Filter(int number)
-    {
-        return number > 2;
-    }
-
-    static bool OddNumbers(int number)
-    {
-        return number % 2 != 0;
-    }
-
-    static int Sum(int first, int second)
-    {
-        return first + second;
-    }
+static void Print(string text)
+{
+    Console.WriteLine(text);
 }
