a1f18432170914cf396399e0bb9165c684476ff8
Martin Marinov <m.marinov1733@gmail.com>
2026-01-29 23:03:12 +0200

    sort even nums

---
+++ 09.Functional-Programming-Lab/01-sort-even-numbers/Program.cs
@@ -0,0 +1,7 @@
+﻿var numbers = Console.ReadLine()
+    .Split(", ")
+    .Select(int.Parse)
+    .Where(n => n % 2 == 0)
+    .OrderBy(n => n);
+
+Console.WriteLine(string.Join(", ", numbers));
---
+++ 09.Functional-Programming-Lab/Playground/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    private static int number = 0;
+    static void Main(string[] args)
+    {
+        var result = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .Where(n => n % 2 == 0)
+            .ToList();
+
+        var numbers = new int[]{ 4, 3, 2, 2, 4, 56, 32, 4 };
+        var filtered = numbers
+            .Where(Filter)
+            .ToList();
+    }
+
+    static bool Filter(int number)
+    {
+        return number > 2;
+    }
+
+    static bool OddNumbers(int number)
+    {
+        return number % 2 != 0;
+    }
+
+    static int Sum(int first, int second)
+    {
+        return first + second;
+    }
+}
