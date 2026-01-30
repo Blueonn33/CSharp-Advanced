6b5b686de59f6340aca2ed57796456bdfdbaf578
Martin Marinov <m.marinov1733@gmail.com>
2026-01-30 23:38:47 +0200

    filter

---
+++ 09.Functional-Programming-Lab/05-filter-by-age/Program.cs
@@ -0,0 +1,67 @@
+﻿int count = int.Parse(Console.ReadLine());
+
+var people = new Dictionary<string, int>();
+
+for (int i = 0; i < count; i++)
+{
+    var pair = Console.ReadLine().Split(", ");
+
+    var name = pair[0];
+    var age = int.Parse(pair[1]);
+
+    people.Add(name, age);
+}
+
+var condition = Console.ReadLine();
+var ageLimit = int.Parse(Console.ReadLine());
+var format = Console.ReadLine();
+
+Func<int, bool> conditionFunc;
+
+if (condition == "younger")
+{
+    conditionFunc = age => age < ageLimit;
+}
+else
+{
+    conditionFunc = age => age >= ageLimit;
+}
+
+var filteredPeople = Filter(people, conditionFunc);
+
+Func<string, int, string> selector;
+
+if (format == "name")
+{
+    selector = (name, age) => name;
+}
+else if (format == "age")
+{
+    selector = (name, age) => age.ToString();
+}
+else
+{
+    selector = (name, age) => $"{name} - {age}";
+}
+
+foreach (var (name, age) in filteredPeople)
+{
+    var result = selector(name, age);
+    Console.WriteLine(result);
+}
+
+static Dictionary<string, int> Filter(Dictionary<string, int> dictionary, Func<int, bool> predicate)
+{
+    var result = new Dictionary<string, int>();
+
+    foreach (var kvp in dictionary)
+    {
+        if (predicate(kvp.Value))
+        {
+            result.Add(kvp.Key, kvp.Value);
+        }
+    }
+
+    return result;
+}
+
