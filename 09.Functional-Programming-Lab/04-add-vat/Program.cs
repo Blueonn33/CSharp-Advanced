51a555a524071e30b7100d6b8208092eaeee4f25
Martin Marinov <m.marinov1733@gmail.com>
2026-01-30 23:20:34 +0200

    vat

---
+++ 09.Functional-Programming-Lab/04-add-vat/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.ReadLine()
+    .Split(", ")
+    .Select(double.Parse)
+    .Select(price => price + price * 20 / 100)
+    .Select(n => $"{n:F2}")
+    .ToList()
+    .ForEach(n => Console.WriteLine(n));
