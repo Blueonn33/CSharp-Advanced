6307ecfa161981baed0bc93cff8198860d0988e8
Martin Marinov <m.marinov1733@gmail.com>
2026-01-31 22:48:21 +0200

    custom min

---
+++ 10.Functional-Programming-Exercise/03-custom-min-function/Program.cs
@@ -0,0 +1,18 @@
+﻿Func<int[], int> smallestFunc = FindSmallestNumber;
+
+int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int FindSmallestNumber(int[] array)
+{
+    int min = int.MaxValue;
+
+    foreach (int item in array)
+    {
+        if (item < min)
+        {
+            min = item;
+        }
+    }
+
+    return min;
+}
