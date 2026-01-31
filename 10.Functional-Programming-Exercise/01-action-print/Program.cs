8f5096f4f9fa2639fd67e06d54532a1357e066b6
Martin Marinov <m.marinov1733@gmail.com>
2026-01-31 22:33:02 +0200

    action print

---
+++ 10.Functional-Programming-Exercise/01-action-print/Program.cs
@@ -0,0 +1,12 @@
+﻿Action<string[]> printer = PrintNames;
+
+string[] input = Console.ReadLine().Split();
+printer(input);
+
+void PrintNames(string[] names)
+{
+    foreach (var name in names)
+    {
+        Console.WriteLine(name);
+    }
+}
