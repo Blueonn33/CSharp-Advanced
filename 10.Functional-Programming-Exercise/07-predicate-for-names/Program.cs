e0dba81cde96805bd057cae2d313ac70e0016783
Martin Marinov <m.marinov1733@gmail.com>
2026-02-01 23:11:42 +0200

    predicate

---
+++ 10.Functional-Programming-Exercise/07-predicate-for-names/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+string[] names = Console.ReadLine()
+    .Split()
+    .ToArray();
+
+Func<string, int, bool> lengthFilter = (s, n) => s.Length <= n;
+
+string[] filteredNames = names.Where(name => lengthFilter(name, n))
+    .ToArray();
+
+Console.WriteLine(string.Join(Environment.NewLine, filteredNames));
