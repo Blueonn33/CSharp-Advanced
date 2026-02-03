1b1a074d8db90a7e07df13c9ff239642006b1c0a
Martin Marinov <m.marinov1733@gmail.com>
2026-02-03 23:36:35 +0200

    trifunction

---
+++ 10.Functional-Programming-Exercise/11-trifunction/Program.cs
@@ -0,0 +1,24 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+string[] names = Console.ReadLine().Split();
+
+foreach (var name in names)
+{
+    if (AreCharactersBigger(name, n))
+    {
+        Console.WriteLine(name);
+        return;
+    }
+}
+
+bool AreCharactersBigger(string input, int n)
+{
+    int characters = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        characters += input[i];
+    }
+
+    return characters > n;
+}
