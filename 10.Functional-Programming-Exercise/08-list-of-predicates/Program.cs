2b2eace0adf6d9ef2a319ddd571f8f6b96919e51
Martin Marinov <m.marinov1733@gmail.com>
2026-02-03 13:35:07 +0200

    list of predicates

---
+++ 10.Functional-Programming-Exercise/08-list-of-predicates/Program.cs
@@ -0,0 +1,32 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] array = Enumerable.Range(1, n).ToArray();
+int[] divisors = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+List<Func<int, bool>> filters = new();
+
+foreach (int divisor in divisors)
+{
+    Func<int, bool> filter = n =>
+    {
+        return n % divisor == 0;
+    };
+
+    filters.Add(filter);
+}
+
+foreach (var filter in filters)
+{
+    List<int> filteredList = new();
+
+    foreach (var number in array)
+    {
+        if (filter(number))
+        {
+            filteredList.Add(number);
+        }
+    }
+
+    array = filteredList.ToArray();
+}
+
+Console.WriteLine(string.Join(" ", array));
