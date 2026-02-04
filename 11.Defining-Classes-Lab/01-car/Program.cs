d214cd336d9dbd2ced99a123499ef8c6e9f38229
Martin Marinov <m.marinov1733@gmail.com>
2026-02-04 15:12:34 +0200

    car

---
+++ 11.Defining-Classes-Lab/01-car/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var car = new Car()
+            {
+                Make = "Aston Martin",
+                Model = "Vulcan",
+                Year = 2026
+            };
+        }
+    }
+}
