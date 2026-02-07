de429a9d49044c224fa91be13c367882ac60f54c
Martin Marinov <m.marinov1733@gmail.com>
2026-02-07 14:39:17 +0200

    array creator

---
+++ 13.Generics-Lab/GenericArrayCreator/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var numbers = ArrayCreator.Create(10, 17);
+            var texts = ArrayCreator.Create(20, "Default");
+        }
+    }
+}
