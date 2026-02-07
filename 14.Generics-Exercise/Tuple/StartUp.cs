03149ee13796a9f3e7663b4468001f2ee545e4f9
Martin Marinov <m.marinov1733@gmail.com>
2026-02-07 18:06:51 +0200

    tuple

---
+++ 14.Generics-Exercise/Tuple/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace Tuple
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            CustomTuple<string, string> nameAndAdress = new();
+            string[] line = Console.ReadLine().Split();
+
+            nameAndAdress.Item1 = line[0] + " " + line[1];
+            nameAndAdress.Item2 = line[2];
+            Console.WriteLine(nameAndAdress);
+
+            CustomTuple<string, int> nameAndBeer = new();
+            line = Console.ReadLine().Split();
+
+            nameAndBeer.Item1 = line[0];
+            nameAndBeer.Item2 = int.Parse(line[1]);
+            Console.WriteLine(nameAndBeer);
+
+            CustomTuple<int, double> numbers = new();
+            line = Console.ReadLine().Split();
+
+            numbers.Item1 = int.Parse(line[0]);
+            numbers.Item2 = double.Parse(line[1]);
+            Console.WriteLine(numbers);
+        }
+    }
+}
