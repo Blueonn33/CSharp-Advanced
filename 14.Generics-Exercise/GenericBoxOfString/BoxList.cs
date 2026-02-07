2d723130120de6097d02e112e0933dc18a8256e1
Martin Marinov <m.marinov1733@gmail.com>
2026-02-07 16:28:50 +0200

    swap method

---
+++ 14.Generics-Exercise/GenericBoxOfString/BoxList.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBoxOfString
+{
+    public class BoxList<T>
+    {
+        public BoxList()
+        {
+            Data = new List<Box<T>>();
+        }
+
+        public List<Box<T>> Data { get; set; }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            Box<T> temp = Data[firstIndex];
+            Data[firstIndex] = Data[secondIndex];
+            Data[secondIndex] = temp;
+        }
+    }
+}
--- 14.Generics-Exercise/GenericBoxOfString/StartUp.cs
+++ 14.Generics-Exercise/GenericBoxOfString/StartUp.cs
@@ -6,12 +6,25 @@ namespace GenericBoxOfString
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
+            BoxList<int> list = new();
 
             for (int i = 0; i < n; i++)
             {
                 string word = Console.ReadLine();
                 Box<int> box = new(int.Parse(word));
 
+                list.Data.Add(box);
+            }
+
+            int[] indexes = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            list.Swap(indexes[0], indexes[1]);
+
+            foreach (var box in list.Data)
+            {
                 Console.WriteLine(box);
             }
         }
