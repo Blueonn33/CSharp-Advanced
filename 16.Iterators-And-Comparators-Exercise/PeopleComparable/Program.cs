d237a5fafe117ce1038657ad3d898e53cc8e7204
Martin Marinov <m.marinov1733@gmail.com>
2026-02-08 19:34:15 +0200

    equality

---
+++ 16.Iterators-And-Comparators-Exercise/PeopleComparable/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PeopleComparable
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Person> people = new();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string name = input[0];
+                int age = int.Parse(input[1]);
+                //string town = input[2];
+                //int id = int.Parse(input[3]);
+
+                people.Add(new Person(name, age, "", -1));
+            }
+
+            Console.WriteLine(people.Count);
+            Console.WriteLine(people.Count);
+        }
+    }
+}
