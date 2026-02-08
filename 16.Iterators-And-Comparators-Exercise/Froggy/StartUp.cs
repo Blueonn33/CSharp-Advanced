4c1dd450192e1fb9eb1f42e8cf59d28a01a1dc10
Martin Marinov <m.marinov1733@gmail.com>
2026-02-08 16:09:43 +0200

    froggy

---
+++ 16.Iterators-And-Comparators-Exercise/Froggy/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Channels;
+
+namespace Froggy
+{
+    internal class StartUp
+    { 
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToList();
+
+            Lake lake = new Lake(input);
+            bool isFirst = true;
+
+            foreach (var stone in lake)
+            {
+                if (isFirst)
+                {
+                    isFirst = false;
+                    Console.Write($"{stone}");
+                }
+                else
+                {
+                    Console.Write($", {stone}");
+                }
+            }
+        }
+    }
+}
