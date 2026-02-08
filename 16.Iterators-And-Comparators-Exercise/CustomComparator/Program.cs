f18c5c09c5e1d88c33f1dfc5632d6be66925d2e9
Martin Marinov <m.marinov1733@gmail.com>
2026-02-08 20:14:42 +0200

    comparator

---
+++ 16.Iterators-And-Comparators-Exercise/CustomComparator/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace CustomComparator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Array.Sort(array, new NumbersComparer());
+
+            Console.WriteLine(string.Join(" ", array));
+        }
+    }
+
+    class NumbersComparer : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            if (x % 2 == 0)
+            {
+                if (y % 2 == 0)
+                {
+                    return x.CompareTo(y);
+                }
+
+                return -1;
+            }
+            else
+            {
+                if (y % 2 == 0)
+                {
+                    return 1;
+                }
+
+                return x.CompareTo(y);
+            }
+        }
+    }
+}
