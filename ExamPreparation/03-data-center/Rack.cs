51967a9580883e79e7754aef02a94b22e0cf384b
Martin Marinov <m.marinov1733@gmail.com>
2026-02-10 15:52:30 +0200

    data center

---
+++ ExamPreparation/03-data-center/Rack.cs
@@ -0,0 +1,71 @@
+﻿using System.Security.AccessControl;
+using System.Text;
+
+namespace DataCenter
+{
+    public class Rack
+    {
+        public Rack(int slots)
+        {
+            Slots = slots;
+            Servers = new List<Server>();
+        }
+
+        public int Slots
+        {
+            get;
+            set;
+        }
+        public List<Server> Servers { get; set; }
+
+        public int GetCount
+        {
+            get { return Servers.Count; }
+        }
+        public void AddServer(Server server)
+        {
+            if (Slots > GetCount && !Servers.Exists(s => s.SerialNumber == server.SerialNumber))
+            {
+                Servers.Add(server);
+            }
+        }
+
+        public bool RemoveServer(string serialNumber)
+        {
+            Server server = Servers.FirstOrDefault(s => s.SerialNumber == serialNumber);
+
+            if (server != null)
+            {
+                Servers.Remove(server);
+                return true;
+            }
+
+            return false;
+        }
+
+        public string GetHighestPowerUsage()
+        {
+            Server server = Servers.MaxBy(s => s.PowerUsage);
+            return server.ToString();
+        }
+
+        public int GetTotalCapacity()
+        {
+            int capacity = Servers.Sum(s => s.Capacity);
+            return capacity;
+        }
+
+        public string DeviceManager()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{GetCount} servers operating:");
+
+            foreach (var server in Servers)
+            {
+                sb.AppendLine(server.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
