f68fd53547e1417592fa6270a989434038194d24
Martin Marinov <m.marinov1733@gmail.com>
2026-02-11 15:39:29 +0200

    merge

--- 07.Streams-Files-And-Directories-Lab/MergeFiles/MergeFiles.cs
+++ 07.Streams-Files-And-Directories-Lab/MergeFiles/MergeFiles.cs
@@ -1,6 +1,7 @@
 ﻿namespace MergeFiles
 {
     using System;
+    using System.Collections.Generic;
     using System.IO;
     public class MergeFiles
     {
@@ -15,6 +16,30 @@
 
         public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
         {
+            using var firstInput = new StreamReader(firstInputFilePath);
+            using var secondInput = new StreamReader(secondInputFilePath);
+            using var writer = new StreamWriter(outputFilePath);
+
+            while (true)
+            {
+                var line1 = firstInput.ReadLine();
+                var line2 = secondInput.ReadLine();
+
+                if (line1 != null)
+                {
+                    writer.WriteLine(line1);
+                }
+
+                if (line2 != null)
+                {
+                    writer.WriteLine(line2);
+                }
+
+                if (line1 == null && line2 == null)
+                {
+                    break;
+                }
+            }
         }
     }
 }
