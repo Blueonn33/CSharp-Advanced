497ed100558f5ee920e29cba8e272fdd27310cd1
Martin Marinov <m.marinov1733@gmail.com>
2026-02-11 23:13:32 +0200

    factorial

---
+++ 17.Basic-Algorithms-Exercise/02-recursive-factorial/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+static long Factorial(int n)
+{
+    if (n == 0)
+        return 1;
+
+    return n * Factorial(n - 1);
+}
+
+Console.WriteLine(Factorial(n));
