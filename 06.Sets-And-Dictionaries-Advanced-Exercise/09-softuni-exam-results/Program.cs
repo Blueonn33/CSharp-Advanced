b5f9e8865fddbf4da5b5aa7ef5cc239e4c15b9e3
Martin Marinov <m.marinov1733@gmail.com>
2026-02-11 22:50:47 +0200

    softuni exam

---
+++ 06.Sets-And-Dictionaries-Advanced-Exercise/09-softuni-exam-results/Program.cs
@@ -0,0 +1,61 @@
+﻿// "{username}-{language}-{points}", until "exam finished". 
+
+string command = Console.ReadLine();
+
+Dictionary<string, int> languageCounts = new();
+Dictionary<string, int> userPoints = new();
+
+while (command != "exam finished")
+{
+    string[] inputParts = command.Split('-', StringSplitOptions.RemoveEmptyEntries);
+    string username = inputParts[0];
+    string language = inputParts[1];
+    int points = 0;
+
+    if (language == "banned")
+    {
+        userPoints.Remove(username);
+    }
+    else
+    {
+        points = int.Parse(inputParts[2]);
+
+        if (!languageCounts.ContainsKey(language) && language != "banned")
+        {
+            languageCounts[language] = 0;
+        }
+
+        if (!userPoints.ContainsKey(username))
+        {
+            userPoints[username] = points;
+        }
+        else
+        {
+            if (userPoints[username] < points)
+            {
+                userPoints[username] = points;
+            }
+        }
+
+        languageCounts[language]++;
+    }
+
+    command = Console.ReadLine();
+}
+
+languageCounts = languageCounts.OrderByDescending(s => s.Value).ThenBy(s => s.Key).ToDictionary(s => s.Key, s => s.Value);
+ userPoints = userPoints.OrderByDescending(s => s.Value).ThenBy(s => s.Key).ToDictionary(s => s.Key, s => s.Value);
+
+Console.WriteLine("Results:");
+
+foreach (var (name, points) in userPoints.Where(u => u.Value > 0))
+{
+    Console.WriteLine($"{name} | {points}");
+}
+
+Console.WriteLine($"Submissions:");
+
+foreach (var (language, count) in languageCounts)
+{
+    Console.WriteLine($"{language} - {count}");
+}
