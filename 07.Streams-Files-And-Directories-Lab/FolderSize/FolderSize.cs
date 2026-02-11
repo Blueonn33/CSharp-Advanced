f3f00f7ae210ac9431a9f39897219cda14914ee6
Martin Marinov <m.marinov1733@gmail.com>
2026-02-11 21:25:17 +0200

    folder size

--- 07.Streams-Files-And-Directories-Lab/FolderSize/FolderSize.cs
+++ 07.Streams-Files-And-Directories-Lab/FolderSize/FolderSize.cs
@@ -14,6 +14,20 @@
 
         public static void GetFolderSize(string folderPath, string outputFilePath)
         {
+            string[] files = Directory.GetFiles(folderPath, "*.*", SearchOption.AllDirectories);
+
+            long totalBytes = 0;
+
+            foreach (string file in files)
+            {
+                FileInfo info = new FileInfo(file);
+                totalBytes += info.Length;
+            }
+
+            double sizeInKb = totalBytes / 1024.0;
+
+            File.WriteAllText(outputFilePath, $"{sizeInKb} KB");
         }
+
     }
 }
