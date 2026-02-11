b9caadfed18e2dcdac58d34ee297ed54e714803d
Martin Marinov <m.marinov1733@gmail.com>
2026-02-11 15:31:15 +0200

    word count

--- 07.Streams-Files-And-Directories-Lab/WordCount/WordCount.cs
+++ 07.Streams-Files-And-Directories-Lab/WordCount/WordCount.cs
@@ -17,6 +17,62 @@
 
         public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
         {
+            using var words = new StreamReader(wordsFilePath);
+            using var reader = new StreamReader(textFilePath);
+            using var writer = new StreamWriter(outputFilePath);
+
+            Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+
+            while (true)
+            {
+                var line = words.ReadLine();
+
+                if (line == null)
+                {
+                    break;
+                }
+
+                string[] wordsInput = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var word in wordsInput)
+                {
+                    string lower = word.ToLower();
+
+                    if (!wordCounts.ContainsKey(lower))
+                    {
+                        wordCounts[lower] = 0;
+                    }
+                }
+            }
+
+            while (true)
+            {
+                var line = reader.ReadLine();
+
+                if (line == null)
+                {
+                    break;
+                }
+
+                string[] wordsInput = line.Split(
+                    new char[] { ',', ' ', '.', '?', '!', '-' },
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var word in wordsInput)
+                {
+                    string lower = word.ToLower();
+
+                    if (wordCounts.ContainsKey(lower))
+                    {
+                        wordCounts[lower]++;
+                    }
+                }
+            }
+
+            foreach (var (word, count) in wordCounts.OrderByDescending(c => c.Value))
+            {
+                writer.WriteLine($"{word} - {count}");
+            }
         }
     }
 }
