cea02f2268a326ed8d71adf5c16ee840f903d557
Martin Marinov <m.marinov1733@gmail.com>
2026-02-11 22:01:51 +0200

    even lines

--- 08.Streams-Files-And-Directories-Exercises/EvenLines/EvenLines.cs
+++ 08.Streams-Files-And-Directories-Exercises/EvenLines/EvenLines.cs
@@ -1,6 +1,8 @@
 ﻿namespace EvenLines
 {
     using System;
+    using System.IO;
+
     public class EvenLines
     {
         static void Main()
@@ -12,7 +14,38 @@
 
         public static string ProcessLines(string inputFilePath)
         {
-            throw new NotImplementedException();
+            int counter = 0;
+            string result = "";
+
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                string line = reader.ReadLine();
+
+                while (line != null)
+                {
+                    string[] lineArr = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    Array.Reverse(lineArr);
+                    char[] symbols = new char[] { '-', ',', '.', '!', '?' };
+
+                    for (int i = 0; i < lineArr.Length; i++)
+                    {
+                        for (int j = 0; j < symbols.Length; j++)
+                        {
+                            lineArr[i] = lineArr[i].Replace(symbols[j].ToString(), "@");
+                        }
+                    }
+
+                    if (counter % 2 == 0)
+                    {
+                        result += string.Join(" ", lineArr) + Environment.NewLine;
+                    }
+
+                    line = reader.ReadLine();
+                    counter++;
+                }
+            }
+
+            return result;
         }
     }
 }
