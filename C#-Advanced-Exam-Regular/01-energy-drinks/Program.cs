2ebcbed6058d0515b6a27357a7c40bef15eec0b7
Martin Marinov <m.marinov1733@gmail.com>
2026-02-14 09:25:03 +0200

    energy drinks

---
+++ C#-Advanced-Exam-Regular/01-energy-drinks/Program.cs
@@ -0,0 +1,36 @@
+﻿Stack<int> caffeine = new(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+Queue<int> energyDrinks = new(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+
+int totalCaffeine = 0;
+
+while (totalCaffeine <= 300 && caffeine.Count > 0 && energyDrinks.Count > 0)
+{
+    int caffeineInput = caffeine.Pop();
+    int energyDrinkInput = energyDrinks.Dequeue();
+    int sum = caffeineInput * energyDrinkInput;
+
+    if (totalCaffeine + sum <= 300)
+    {
+        totalCaffeine += sum;
+    }
+    else
+    {
+        if (totalCaffeine >= 30)
+        {
+            totalCaffeine -= 30;
+        }
+
+        energyDrinks.Enqueue(energyDrinkInput);
+    }
+}
+
+if (energyDrinks.Count > 0)
+{
+    Console.WriteLine($"Drinks left: {string.Join(", ", energyDrinks)}");
+}
+else if (totalCaffeine <= 300)
+{
+    Console.WriteLine($"At least Stamat wasn't exceeding the maximum caffeine.");
+}
+
+Console.WriteLine($"Stamat is going to sleep with {totalCaffeine} mg caffeine.");
